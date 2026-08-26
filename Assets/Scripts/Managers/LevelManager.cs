486e6eb7cdf3f5c42126025cbe6a52022e6cbbe0
Johanna Jocher <jojocher@stud.mediadesign.de>
2026-08-26 13:41:47 +0200

    feat: Score display and Basic UI
    add UI Elements
    - Game Over Screen
    - Game Won Screen
    - Highscore Screen
    - Level Information (added Ball Count)
    
    Preparation of UI Interactions
     - Game Over
      -Back To Menu Button
     - You Won
      - Name Input + Logic
      - Submit Button + Logic
     - Highscore
      - Score Table + Logic
      - Back to Menu
     - Level Information + Logic
     - Highscore Button + Logic

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -5,13 +5,32 @@ using TMPro;
 public class LevelManager : MonoBehaviour
 {
    [SerializeField] TMP_Text BrickCounter;
+   [SerializeField] TMP_Text BallCounter;
+
+   [SerializeField] GameObject GameOverUI;
+    [SerializeField] GameObject GameWonUI;
+    [SerializeField] GameObject HighscoreScreenUI;
+    [SerializeField] GameObject MenuScreenUI;
+    [SerializeField] GameObject PlayUI;
+
+    [SerializeField] TMP_Text SubmittedText;
+    [SerializeField] TMP_Text SubmittedTextFailed;
+
+   // ScoreManager scoreManager;
+    [SerializeField] TMP_Text scoreNumberText;
 
     int m_iBrickCountOld;
+    int m_iBallCountOld;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+     //   scoreManager = this.gameObject.GetComponent<ScoreManager>().Instance;
+    }
     void Start()
     {
         BrickCounterText();
-       
+        BallCounterText();
+        ScoreCounterText();
     }
 
     // Update is called once per frame
@@ -19,14 +38,115 @@ public class LevelManager : MonoBehaviour
     {
         if (Brick.bricks.Count != m_iBrickCountOld)
             BrickCounterText();
+        
+        
+        if (Ball.balls.Count != m_iBallCountOld)
+            BallCounterText();
+    
+
+        if(Brick.bricks.Count <= 0)
+        {
+            //gewonnen
+            //
+           // gameObject.GetComponent<ScoreManager>().m_bTimerActive = false;
+           // gameObject.GetComponent<ScoreManager>().m_iScore;
+
+        }
     }
 
 
     void BrickCounterText()
     {
         m_iBrickCountOld = Brick.bricks.Count;
-        BrickCounter.text = "Verbleibende Blöcke: " + Brick.bricks.Count;
+        BrickCounter.text = " " + Brick.bricks.Count;
     }
 
+    void BallCounterText()
+    {
+        m_iBallCountOld = Ball.balls.Count;
+        BallCounter.text = " " + Ball.balls.Count;
+    }
+
+    void ScoreCounterText()
+    {
+        scoreNumberText.text = " " + ScoreManager.Instance.m_iScore;
+        
+        
+    }
+
+    public void GameWonSubmit()
+    {
+
+        if (SubmittedText.text.Trim().Length >= 2)
+        {
+            ScoreManager.Instance.PointToList(SubmittedText.text);
+
+
+            HighscoreScreen(true, false);
+            Debug.Log("submittet text: " + SubmittedText.text);
+
+            
+        }
+        else
+            SubmittedTextFailed.gameObject.SetActive(true);
+    }
     
+    public void PlayScreen(bool _is)
+    {
+        PlayUI.SetActive(_is);
+    }
+
+    public void GameOver(bool _is)
+    {
+        GameOverUI.SetActive(_is);
+
+    }
+
+    public void GameWon(bool _is)
+    {
+        GameWonUI.SetActive(_is);
+    }
+
+    public void MenuScreen(bool _is)
+    {
+        MenuScreenUI.SetActive(_is);
+    }
+
+    public void HighscoreScreen(bool _isA, bool _isM)
+    {
+        HighscoreScreenUI.SetActive(_isA);
+        if (_isA)
+        {
+
+            List<string> _hs = ScoreManager.Instance.PointsFromList();
+
+            GameObject[] goListe = HighscoreScreenUI.GetComponent<HighscoreUI>().liste;
+
+            for(int i = 0; i < _hs.Count; i++)
+            {
+                goListe[i].GetComponent<TMP_Text>().text = _hs[i];
+            }
+
+            if (_isM == false)
+            {
+                int iRank = ScoreManager.Instance.LastEntryInRanking();
+                int iSCore = ScoreManager.Instance.m_iScore;
+
+                goListe[10].gameObject.SetActive(true);
+                goListe[10].GetComponent<TMP_Text>().text = "Your Rank: " + iRank + " Your Score: " + iSCore;
+            }
+            else
+            {
+               // goListe[10].GetComponent<TMP_Text>().text = " ";
+                goListe[10].gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    //public void HighscoreScreen(bool _isA)
+    //{
+    //    HighscoreScreenUIl.SetActive(_isA);
+    //}
+
 }
